f01cc49132675b4f69d7aefaa8e01026599d1c90
W51109 <damiantelesz@gmail.com>
2018-05-26 13:02:16 +0200

    Pobieranie danych

--- app/src/main/java/com/example/stud/musicapp/api/apiClient.java
+++ app/src/main/java/com/example/stud/musicapp/api/apiClient.java
@@ -1,6 +1,14 @@
 package com.example.stud.musicapp.api;
 
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface apiClient {
+
+    @GET( "trending.php" )
+    Call<trendingList> getTrendingList(@Query( "country" ) String country, @Query ( "type" )
+            String type, @Query ( "format" ) String format);
 
 }
--- app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsActivity.java
+++ app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsActivity.java
@@ -1,9 +1,19 @@
 package com.example.stud.musicapp.topsongs;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.stud.musicapp.R;
+import com.example.stud.musicapp.api.apiService;
+import com.example.stud.musicapp.api.trendingList;
+import com.google.gson.Gson;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class TopSongsActivity extends AppCompatActivity {
 
@@ -13,6 +23,24 @@ public class TopSongsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_top_songs);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+        Call<trendingList> trendingListCall = apiService. getService ().getTrendingList( "us" ,
+                "itunes" , "singles" );
+        trendingListCall.enqueue( new Callback<trendingList>() {
+            @Override
+            public void onResponse(@NonNull Call<trendingList> call, @NonNull
+                    Response<trendingList> response) {
+                trendingList trendingList = response.body();
+                Log. d ( "TAG" , new Gson().toJson(trendingList));
+            }
+            @Override
+            public void onFailure( @NonNull Call<trendingList> call, Throwable t) {
+                Toast. makeText (TopSongsActivity. this , "Blad pobierania danych: " +
+                        t.getLocalizedMessage(), Toast. LENGTH_SHORT ).show();
+            }
+        });
 
     }
 
